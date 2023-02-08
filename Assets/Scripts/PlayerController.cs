347fa0a2d5085442ae6ff5e99b242f97fa0d3dad
Darío Cabezas <81370538+Zethearc@users.noreply.github.com>
2023-02-08 12:51:25 -0500

    Segundo Avance

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,29 +5,53 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 4.0f; // Velocidad de movimiento
 
-    private const string horizontal = "Horizontal"; // Conexión a la tecla asignada
-    private const string vertical = "Vertical";  // Conexión a la tecla asignada
+    public float speed = 4.0f;
+    private bool walking = false;
+    public Vector2 lastMovement = Vector2.zero;
+
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+    private const string lastHorizontal = "LastHorizontal";
+    private const string lastVertical = "LastVertical";
+    private const string walkingState = "Walking";
+
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // s = v*t distancia es igual a velocidad por tiempo
+        // s = v*t;
+        walking = false;
 
-        if(Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
+        if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
         {
-            this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal)*speed*Time.deltaTime, 0, 0));
+            this.transform.Translate(
+                new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime,0,0));
+            walking = true;
+            lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
         }
-        if(Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
+
+        if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
         {
-            this.transform.Translate(new Vector3(0, Input.GetAxisRaw(vertical)*speed*Time.deltaTime, 0));
+            this.transform.Translate(
+                new Vector3(0, Input.GetAxisRaw(vertical)*speed*Time.deltaTime,0));
+            walking = true;
+            lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
         }
+
+        animator.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
+        animator.SetFloat(vertical, Input.GetAxisRaw(vertical));
+
+        animator.SetBool(walkingState, walking);
+
+        animator.SetFloat(lastHorizontal, lastMovement.x);
+        animator.SetFloat(lastVertical, lastMovement.y);
     }
 }
